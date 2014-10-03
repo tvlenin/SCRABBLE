a9d12c00b1afacebb2b637f904cf3ddab5db4564
tvlenin <tvlenin@UbuntuLenin>
2014-10-03 05:58:13 -0600

    Merge origin/Fabricio into lenin

--- src/scrabble/LOGIC/CampoTablero.java
+++ src/scrabble/LOGIC/CampoTablero.java
@@ -6,7 +6,15 @@ public class CampoTablero {
     private int valorFicha;     //valor que tenga la letra que se coloco en este campo
     private String descripcion;
     private int pos;            //posicion respecto al tablero
-    
+     
+    /**
+     * Metodo constructor de la clase CampoTablero, en el cual se evaluan los aspecto relacionados con los espacios del tablero,
+     * Ya sea si el espacio se encuentra disponible, si el espacio cuenta con alguna bonificacion
+     * @param pPos Posicion del espacio que se esta evalundo con respecto al tablero
+     * @param pMultiplicador Bonos de puntuacion que posea el espacio que se esta evaluando
+     * @param pDescrip Tipo de espacio que se esta evaluando, para saber si tiene bonificacion de palabra, de letra o sin bonificacion
+     * @author tvlnin
+     */
     public CampoTablero( int pPos, int pMultiplicador, String pDescrip){
         this.multiple = pMultiplicador;
         this.ficha = null;
